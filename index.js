68de5084799c38c879c3869369bcd80e4a82777b
Michael Daof <michael.ivanhoe@gmail.com>
2016-08-29 19:00:32 -0700

    Prepping for deployment

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ var router = require('./router.js');
 var app = express();
 
 app.use('/',  express.static(__dirname + '/'));
-mongoose.connect('mongodb://localhost/directives');
+
+// TODO: Prep DB 
+// var URI = process.env.MONGODB_URI || 'mongodb://localhost/test'
+// mongoose.connect(URI);
 router(app);
 
 var port = process.env.PORT || 3000;
